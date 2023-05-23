233cbbe655ebd34a249be7bff3ff9eb995886e6f
cValtulini <c.valtulini002@studenti.unibs.it>
2023-05-23 11:03:34 +0200

    Modified to create q_true set and from that the different n_realization k (covariance) sets

--- LapMatPred/dataUtilities.py
+++ LapMatPred/dataUtilities.py
@@ -15,9 +15,12 @@ from sklearn.utils import class_weight
 ################################################################################
 # CONSTANTS
 ################################################################################
-FEATURES_DESCRIPTION = feature_description = {
-    "covariance_raw": tf.io.FixedLenFeature([], tf.string),
-    "q_true_raw": tf.io.FixedLenFeature([], tf.string),
+Q_FEATURES_DESCRIPTION = {
+    "q_raw": tf.io.FixedLenFeature([], tf.string),
+}
+
+K_FEATURES_DESCRIPTION = {
+    "k_raw": tf.io.FixedLenFeature([], tf.string),
 }
 
 
@@ -28,128 +31,120 @@ def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-def create_sample(
-    rng, nodes_number, connection_probability, number_of_realizations, q=None
-):
-    # TODO: Modify doc (copy pasted from create_dataset)
+def create_covariance_sample(rng, q_true, number_of_realizations):
+    # TODO: write
     """
-    Creates a dataset from graphs with the specified number of nodes and connection
-    probability following the erdosRenyi model. The dataset is composed by the
-    estimated covariance matrix for the graph signal generated following a multivariate
-    gaussian distribution with 0-mean vector and covariance equal to the inverse of the
-    laplacian for the graph + the identity matrix, which is the ground truth
+    Doc
 
     Parameters
     ----------
-    rng: numpy.random.Generator
-        The random number generator for all random operations performed in this function
-    nodes_number: int
-        The number of nodes for the graphs
-    connection_probability: float
-        The connection probability for the graphs
+    rng:
+    q_true:
     number_of_realizations: int
         The number of signals to generate for each estimation of the covariance matrix
-    set_size: int
-        The number of samples to generate
 
     Returns
     -------
     numpy.ndarray: shape=(set_size, nodes_number, nodes_number)
         The estimated covariance matrices
-    numpy.ndarray: shape=(set_size, nodes_number, nodes_number)
-        The ground truths generalized laplacian matrices
 
     """
-    if q is None:
-        adjacency = erdos_renyi(nodes_number, connection_probability, rng=rng)
-        laplacian = graph_laplacian(adjacency)
-
-        q_true = np.eye(nodes_number) + laplacian
-    else:
-        q_true = q
-
     k_matrix = np.linalg.inv(q_true)
 
-    # mean_vector = np.zeros(nodes_number)
     covariance = np.cov(
         rng.multivariate_normal(
-            np.zeros(nodes_number), k_matrix, size=number_of_realizations
+            np.zeros(k_matrix.shape[0]), k_matrix, size=number_of_realizations
         ),
         rowvar=False,
     )
-    return covariance, q_true
+    return covariance
 
 
-def create_tfr_set(
-    rng,
-    nodes_number,
-    connection_probability,
-    number_of_realizations,
-    set_size,
-    path,
-    repetitions=1,
-):
+def create_q_true_tfr_set(rng, nodes_number, connection_probability, set_size, path):
     """Creates a dataset in the tfrecords format."""
 
     filename = os.path.join(path + ".tfrecords")
     print("Writing", filename)
 
     writer = tf.io.TFRecordWriter(filename)
-    for index in range(set_size // repetitions):
-        sample_zero = create_sample(
-            rng, nodes_number, connection_probability, number_of_realizations
+    for index in range(set_size):
+        laplacian = graph_laplacian(
+            erdos_renyi(nodes_number, connection_probability, rng=rng)
         )
-        samples = [
-            create_sample(
-                rng,
-                nodes_number,
-                connection_probability,
-                number_of_realizations,
-                q=sample_zero[1],
+
+        q = np.eye(nodes_number) + laplacian
+
+        q_tf = tf.convert_to_tensor(q)
+        q_raw = tf.io.serialize_tensor(q_tf)
+
+        example = tf.train.Example(
+            features=tf.train.Features(
+                feature={
+                    "q_raw": _bytes_feature(q_raw.numpy()),
+                }
             )
-            for _ in range(1, repetitions)
-        ]
-        samples.insert(0, sample_zero)
-
-        for sample in samples:
-            covariance_tf = tf.convert_to_tensor(sample[0])
-            q_true_tf = tf.convert_to_tensor(sample[1])
-
-            covariance_raw = tf.io.serialize_tensor(covariance_tf)
-            q_true_raw = tf.io.serialize_tensor(q_true_tf)
-
-            example = tf.train.Example(
-                features=tf.train.Features(
-                    feature={
-                        "covariance_raw": _bytes_feature(covariance_raw.numpy()),
-                        "q_true_raw": _bytes_feature(q_true_raw.numpy()),
-                    }
-                )
-            )
-            writer.write(example.SerializeToString())
+        )
+        writer.write(example.SerializeToString())
     writer.close()
 
 
-def _parse_function(example_proto):
+def create_covariance_tfr_set(
+    rng,
+    q_true_set,
+    number_of_realizations,
+    save_path,
+):
+    """Creates a dataset in the tfrecords format."""
+
+    filename = os.path.join(save_path + ".tfrecords")
+    print("Writing", filename)
+
+    writer = tf.io.TFRecordWriter(filename)
+
+    for q in q_true_set:
+        k = create_covariance_sample(rng, q, number_of_realizations)
+
+        k_tf = tf.convert_to_tensor(k)
+        k_raw = tf.io.serialize_tensor(k_tf)
+
+        example = tf.train.Example(
+            features=tf.train.Features(
+                feature={
+                    "k_raw": _bytes_feature(k_raw.numpy()),
+                }
+            )
+        )
+        writer.write(example.SerializeToString())
+    writer.close()
+
+
+def _parse_true_function(example_proto):
     # Parse the input `tf.train.Example` proto using the dictionary above.
 
-    parsed_record = tf.io.parse_single_example(example_proto, FEATURES_DESCRIPTION)
+    parsed_record = tf.io.parse_single_example(example_proto, Q_FEATURES_DESCRIPTION)
 
-    covariance = tf.io.parse_tensor(
-        parsed_record["covariance_raw"], out_type=tf.float64
-    )
-    q_true = tf.io.parse_tensor(parsed_record["q_true_raw"], out_type=tf.float64)
+    q = tf.io.parse_tensor(parsed_record["q_raw"], out_type=tf.float64)
 
-    return covariance, q_true
+    return q
 
 
-def _map_triu_function(covariance, q_true):
-    ones = tf.ones_like(q_true)
+def _parse_k_function(example_proto):
+    # Parse the input `tf.train.Example` proto using the dictionary above.
+
+    parsed_record = tf.io.parse_single_example(example_proto, K_FEATURES_DESCRIPTION)
+
+    k = tf.io.parse_tensor(parsed_record["k_raw"], out_type=tf.float64)
+
+    return k
+
+
+def _map_triu_function(q):
+    ones = tf.ones_like(q)
     mask_a = tf.linalg.band_part(ones, 0, -1)  # Upper triangular matrix of 0s and 1s
     mask_b = tf.linalg.band_part(ones, 0, 0)  # Diagonal matrix of 0s and 1s
     mask = tf.cast(mask_a - mask_b, dtype=tf.bool)  # Make a bool mask
     mask.set_shape([None, None])
 
-    q_true_flat = tf.boolean_mask(q_true, mask)
+    q_triu_flat = tf.boolean_mask(q, mask)
 
-    return covariance, tf.math.abs(q_true_flat)
+    return tf.math.abs(q_triu_flat)
